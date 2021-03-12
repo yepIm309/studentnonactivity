b8dab72811d122595be78a083503b230f5d86511
Ivan Makhinya <78958090+yepIm309@users.noreply.github.com>
2021-03-12 23:40:29 +0300

    Add files via upload

---
+++ Quest1.py
@@ -0,0 +1,8 @@
+a = input("Enter a proizvolnoiy number ")
+b = input("Enter a border number ")
+if b>a:
+    print("Произвольное число меньше пограничного")
+elif b<a:
+    print("Произвольное число больше пограничного")
+else:
+    print("Произвольное число равно пограничному")
