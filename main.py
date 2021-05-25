9e467bd949dfd670fdbd149ebf0fd7aadcfd1570
Ivan Makhinya <78958090+yepIm309@users.noreply.github.com>
2021-05-25 20:26:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello, World")
+title_of_veg = []
+amounts_of_veg = []
+for count in range(0,3):
+    word = input("Введите название овощей")
+    title_of_veg.append(word)
+
+    for str in title_of_veg:
+        print (str.lower())
+    for str in title_of_veg:
+        print (str.upper())
+    for str in title_of_veg:
+        print (str.title())
+
+        for count in range (0, len(title_of_veg)):
+            amount = int(input("Введите количество овощей"))
+            amounts_of_veg.append(amount)
+        for count in range(0, len(title_of_veg)):
+            print("В ящике лежит {amount} {title}. ".format (title=title_of_veg[count], amount = amounts_of_veg[count]))
