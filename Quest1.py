483666a5f9dc9264957b1bf0a8d5a44cf2a1b96c
Ivan Makhinya <78958090+yepIm309@users.noreply.github.com>
2021-06-04 17:00:04 +0300

    Update Quest1.py

--- Quest1.py
+++ Quest1.py
@@ -2,6 +2,8 @@ a = input("Enter a proizvolnoiy number ")
 b = input("Enter a border number ")
 if b>a:
     print("Произвольное число меньше пограничного")
+elif b * 3 < a:
+    print("Произвольное число больше пограничного в 3 раза")
 elif b<a:
     print("Произвольное число больше пограничного")
 else:
